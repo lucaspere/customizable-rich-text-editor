4cfc7c9384a8bd5fe9f8f497114c9332f16080ee
Lucas Pereira <lucasfapereira@gmail.com>
2020-02-17 17:51:25 -0300

    adicionando o Editor

--- src/components/richTextEditor.jsx
+++ src/components/richTextEditor.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faBold, faCode, faItalic } from '@fortawesome/free-solid-svg-icons';
+import { ÈditorState, EditorState } from 'draft-js';
 
 import './richTextEditor.css';
 
 library.add(faBold, faCode, faItalic)
 
 const RichTextEditor = () => {
-   
-   return(
+   const [editorState, setEditorState] = useState(
+      EditorState.createEmpty();
+   )
+
+   return (
       <div>
          oi
       </div>
